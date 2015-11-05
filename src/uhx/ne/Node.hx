5f0c173243bd7c976dff722f9f1d1a4346febcbf
Skial Bainn <skial.bainn@gmail.com>
2015-11-05 14:02:09 +0000

    Add node types.

--- src/uhx/ne/Node.hx
+++ src/uhx/ne/Node.hx
@@ -12,6 +12,15 @@ import uhx.ne.impl.InstructionNode;
  * @author Skial Bainn
  */
 @:forward abstract Node(INode) from INode to INode {
+	
+	public static inline var ELEMENT_NODE:Int = 1;
+	public static inline var TEXT_NODE:Int = 3;
+	public static inline var PROCESSING_INSTRUCTION_NODE:Int = 7;
+	public static inline var COMMENT_NODE:Int = 8;
+	public static inline var DOCUMENT_NODE:Int = 9;
+	public static inline var DOCUMENT_TYPE_NODE:Int = 10;
+	public static inline var DOCUMENT_FRAGMENT_NODE:Int = 11;
+	
 	public inline function new(v) this = v;
 	
 	@:from @:noCompletion public static inline function fromToken(v:Token<HtmlKeywords>) return switch (v) {
