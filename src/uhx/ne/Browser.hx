e1f01d7531282ea86b2622c2ac2d1df2a8e9e95a
Skial Bainn <skial.bainn@gmail.com>
2015-11-07 18:57:13 +0000

    Flesh out uhx.ne.Browser.hx

---
+++ src/uhx/ne/Browser.hx
@@ -0,0 +1,32 @@
+package uhx.ne;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class Browser {
+
+	public static var console:Dynamic;
+	public static var document:Dynamic;
+	public static var location:Dynamic;
+	public static var navigator:Dynamic;
+	public static var supported:Dynamic;
+	public static var window:Dynamic;
+	
+	public static function alert(value:Dynamic):Void {
+		
+	}
+	
+	public static function createXMLHttpRequest() {
+		
+	}
+	
+	public static function getLocalStorage() {
+		
+	}
+	
+	public static function getSessionStorage() {
+		
+	}
+	
+}
